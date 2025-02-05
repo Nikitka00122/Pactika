61ecd96af29a1fe3072ebe5c217888796969f819
nikita <012nikita_012@mail.ru>
2025-02-05 13:21:27 +0700

    практическая работа 4

--- js/main.js
+++ js/main.js
@@ -6,8 +6,21 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         inStock: true,
         inventory: 100,
-        onSale: true
+        onSale: true,
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ]
+
     }
 })
